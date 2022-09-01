b8e481d570d14e682a5eaeaa0e647810a6652fde
Dillon Hess <dhess268@gmail.com>
2022-09-01 19:38:46 -0400

    add try catch block
    When the user inputs a 17 digit number and it does not correspond to a steam Id the app will alert the user that the id is invalid

--- src/components/MyListRow.js
+++ src/components/MyListRow.js
@@ -1,23 +1,25 @@
-import PropTypes from 'prop-types';
+// NOT CURRENTLY USED
 
-export default function MyListRow({ name, developer, gameId }) {
-  return (
-    <div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>{name}</th>
-            <th>{gameId}</th>
-            <th>{developer}</th>
-          </tr>
-        </thead>
-      </table>
-    </div>
-  );
-}
+// import PropTypes from 'prop-types';
 
-MyListRow.propTypes = {
-  name: PropTypes.string.isRequired,
-  developer: PropTypes.string.isRequired,
-  gameId: PropTypes.number.isRequired,
-};
+// export default function MyListRow({ name, developer, gameId }) {
+//   return (
+//     <div>
+//       <table className="table">
+//         <thead>
+//           <tr>
+//             <th>{name}</th>
+//             <th>{gameId}</th>
+//             <th>{developer}</th>
+//           </tr>
+//         </thead>
+//       </table>
+//     </div>
+//   );
+// }
+
+// MyListRow.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   developer: PropTypes.string.isRequired,
+//   gameId: PropTypes.number.isRequired,
+// };
